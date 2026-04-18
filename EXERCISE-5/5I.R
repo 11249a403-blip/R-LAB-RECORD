c5c0520194429f91bd2450d825363c25cd5d22c4
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 20:47:29 +0530

    Create 5I.R

---
+++ EXERCISE-5/5I.R
@@ -0,0 +1,12 @@
+# 9. Student Name Abbreviation
+# Scenario:
+# Display first 3 characters of names.
+
+# Question:
+# Create abbreviations.
+
+names <- c("Bhavya", "Karthik", "Anitha")
+
+abbr <- substr(names, 1, 3)
+
+abbr
