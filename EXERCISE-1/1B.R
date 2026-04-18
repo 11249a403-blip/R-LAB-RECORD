eb6ffab41d2db7638707a8926e580471d1c067cc
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 18:44:17 +0530

    Create 1B.R

---
+++ EXERCISE-1/1B.R
@@ -0,0 +1,16 @@
+# 2. Employee Salary Record
+# Scenario:
+# A company wants to store employee ID, monthly salary, department name, and permanent employee status.
+
+# Question:
+# Write an R program using appropriate data types to store the information.
+
+emp_id <- 6205
+salary <- 52500.80
+department <- "HR"
+is_permanent <- TRUE
+
+emp_id
+salary
+department
+is_permanent
