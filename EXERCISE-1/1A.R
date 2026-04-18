e9bc4fbe9064379b0072ea9bbfbe6f6aaaee1ef4
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 18:36:28 +0530

    Add R program for student information system
    This R program stores and displays student information including roll number, name, CGPA, and pass status.

---
+++ EXERCISE-1/1A.R
@@ -0,0 +1,16 @@
+# 1. Student Information System
+# Scenario:
+# A college wants to store a student's roll number, name, CGPA, and pass status.
+
+# Question:
+# Identify suitable data types and write an R program to store and display the values.
+
+roll_no <- 205
+name <- "Bhavya"
+cgpa <- 9.12
+pass <- TRUE
+
+roll_no
+name
+cgpa
+pass
