e81d0967a8966220af0e684a19cfbc071649542f
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 20:53:55 +0530

    Create 6A.R

---
+++ EXERCISE-6/6A.R
@@ -0,0 +1,16 @@
+# 1. Student Marks Processing (Vectors)
+# Scenario:
+# A lecturer stores marks of students and calculates total, average, and highest.
+
+# Question:
+# Use a suitable data structure and perform calculations.
+
+marks <- c(88, 92, 79, 85, 90)
+
+total <- sum(marks)
+average <- mean(marks)
+highest <- max(marks)
+
+total
+average
+highest
