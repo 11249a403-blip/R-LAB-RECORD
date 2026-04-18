e2e1d8ab0c349c0fd5dd973e17df68b4b44f2241
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 19:29:35 +0530

    Add R program for university admission decision

---
+++ EXERCISE-2/2P.R
@@ -0,0 +1,13 @@
+# 16. University Admission Screening
+# Question:
+# Write an R program for admission decision.
+
+entrance_score <- 88
+twelfth_marks <- 91
+age <- 19
+
+if(entrance_score >= 70 & twelfth_marks >= 75 & age <= 25){
+print("Admission Granted")
+} else {
+print("Admission Rejected")
+}
