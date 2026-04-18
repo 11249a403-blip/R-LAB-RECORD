d663ccf15c8d1b2b786b825653e8afc1c522911a
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 20:59:27 +0530

    Create 6J.R

---
+++ EXERCISE-6/6J.R
@@ -0,0 +1,23 @@
+# 10. Real-World Summary Question
+# Scenario:
+# Demonstrate use of vector, list, and data frame.
+
+# Question:
+# Explain with example.
+
+scores <- c(85, 90, 88)
+
+profile <- list(
+  Name = "Bhavya",
+  Age = 20,
+  Scores = scores
+)
+
+records <- data.frame(
+  Subject = c("Math", "CS", "Physics"),
+  Marks = scores
+)
+
+scores
+profile
+records
