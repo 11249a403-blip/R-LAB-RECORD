0f9c28b89a894f19797f42dd5a72d2ff287f6b10
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 18:55:33 +0530

    CREATE 1E

---
+++ EXERCISE-1/1E.R
@@ -0,0 +1,16 @@
+# 5. Examination Result Processing
+# Scenario:
+# An exam cell stores subject name, total marks, marks obtained, and result status.
+
+# Question:
+# Write an R program using suitable variables and data types.
+
+subject <- "Physics"
+total_marks <- 100
+marks_obtained <- 86
+result <- marks_obtained >= 40
+
+subject
+total_marks
+marks_obtained
+result
