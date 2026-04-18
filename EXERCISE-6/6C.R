ac017db9e02ee433b071724d02f3d122b586e9f0
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 20:55:10 +0530

    Create 6C.R

---
+++ EXERCISE-6/6C.R
@@ -0,0 +1,17 @@
+# 3. Employee Profile Storage (Lists)
+# Scenario:
+# Store employee details with mixed data types.
+
+# Question:
+# Use a suitable data structure.
+
+employee <- list(
+  ID = 101,
+  Name = "Bhavya",
+  Salary = 45000,
+  Department = "IT"
+)
+
+employee
+employee$Name
+employee$Salary
