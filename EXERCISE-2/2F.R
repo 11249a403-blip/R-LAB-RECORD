1ee5d89f37eb77867dc34a81c9667ddeaa241875
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 19:18:11 +0530

    Add password validation program in R

---
+++ EXERCISE-2/2F.R
@@ -0,0 +1,11 @@
+# 6. Password Validation
+# Question:
+# Write an R program using operators and decision making.
+
+password <- "bhavya456"
+
+if(nchar(password) >= 8){
+print("Valid Password")
+} else {
+print("Invalid Password")
+}
