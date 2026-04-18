bbf2dcdc8be182c5f30e5aaa4d9939e52d428545
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 19:38:49 +0530

    Create 3F.R

---
+++ EXERCISE-3/3F.R
@@ -0,0 +1,10 @@
+# 6. Temperature Conversion System
+# Question:
+# Write an R function to perform the conversion.
+
+celsius_to_fahrenheit <- function(c){
+f <- (c * 9/5) + 32
+return(f)
+}
+
+celsius_to_fahrenheit(28)
