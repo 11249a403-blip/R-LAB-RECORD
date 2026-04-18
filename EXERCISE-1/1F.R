a7024c12d57e0efd537c76c3b93e499d0802dee1
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 19:05:25 +0530

    Add banking application example in R
    Implemented a banking application scenario in R with account details.

---
+++ EXERCISE-1/1F.R
@@ -0,0 +1,16 @@
+# 6. Banking Application
+# Scenario:
+# A bank stores account number, account holder name, balance, and active account status.
+
+# Question:
+# Select suitable data types and implement using R.
+
+account_no <- 987654321
+holder_name <- "Bhavya"
+balance <- 35750.75
+account_active <- TRUE
+
+account_no
+holder_name
+balance
+account_active
