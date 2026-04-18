ff150bc705cc3c6e3c531f09784f6fad9b94ad9c
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 19:26:11 +0530

    Add R program for employee appraisal eligibility check

---
+++ EXERCISE-2/2L.R
@@ -0,0 +1,13 @@
+# 12. Employee Performance Appraisal System
+# Question:
+# Write an R program to check appraisal eligibility.
+
+attendance <- 95
+performance <- 9.1
+project_completed <- TRUE
+
+if(attendance >= 90 & performance >= 8 & project_completed){
+print("Eligible for Appraisal")
+} else {
+print("Not Eligible for Appraisal")
+}
