a5e9ba4c52826b9910c373cd4f54ea7aa6cd707f
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 20:56:24 +0530

    Create 6E.R

---
+++ EXERCISE-6/6E.R
@@ -0,0 +1,15 @@
+# 5. College Student Database (Data Frames)
+# Scenario:
+# Store student details in tabular form.
+
+# Question:
+# Use data frame to store structured data.
+
+students <- data.frame(
+  RollNo = c(1, 2, 3),
+  Name = c("Bhavya", "Karthik", "Meena"),
+  Dept = c("IT", "ECE", "CSE"),
+  Marks = c(85, 78, 92)
+)
+
+students
