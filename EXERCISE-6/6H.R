33fee2ba1d5f69e567525eec89c0440b1b1534cd
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 20:58:16 +0530

    Create 6H.R

---
+++ EXERCISE-6/6H.R
@@ -0,0 +1,22 @@
+# 8. Online Exam System
+# Scenario:
+# Store subject marks, student profile, and results.
+
+# Question:
+# Use multiple data structures.
+
+subject_marks <- c(78, 85, 88)
+
+student_profile <- list(
+  Name = "Bhavya",
+  RollNo = 105,
+  Marks = subject_marks
+)
+
+class_results <- data.frame(
+  RollNo = c(101, 102, 103),
+  Total = c(240, 255, 270)
+)
+
+student_profile
+class_results
