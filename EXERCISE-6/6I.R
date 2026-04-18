e249979c56ff5fa2f602bcc9c65b92c29b6d61a3
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 20:58:54 +0530

    Create 6I.R

---
+++ EXERCISE-6/6I.R
@@ -0,0 +1,22 @@
+# 9. Bank Account Management
+# Scenario:
+# Store balances, customer details, and transactions.
+
+# Question:
+# Use vector, list, and data frame.
+
+balances <- c(5000, 5200, 5100)
+
+customer <- list(
+  AccountNo = 12345,
+  Name = "Bhavya"
+)
+
+transactions <- data.frame(
+  Date = c("01-01", "02-01", "03-01"),
+  Amount = c(-500, 1000, -200)
+)
+
+balances
+customer
+transactions
