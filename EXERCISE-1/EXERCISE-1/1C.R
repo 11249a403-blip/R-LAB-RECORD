8d9d1d6eff506facd076559f123b32640a504d26
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 18:48:44 +0530

    Create 1C.R

---
+++ EXERCISE-1/EXERCISE-1/1C.R
@@ -0,0 +1,16 @@
+# 3. Online Shopping Cart
+# Scenario:
+# An e-commerce application needs to store product price, product name, available stock, and discount eligibility.
+
+# Question:
+# Choose suitable data types and write an R program.
+
+product_name <- "Smartphone"
+price <- 28999
+stock_available <- 18
+discount_eligible <- TRUE
+
+product_name
+price
+stock_available
+discount_eligible
