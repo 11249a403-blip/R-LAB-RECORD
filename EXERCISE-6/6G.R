36b02249f2068c021303fb11a5eef8d1dbae3a2b
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 20:57:44 +0530

    Create 6G.R

---
+++ EXERCISE-6/6G.R
@@ -0,0 +1,23 @@
+# 7. E-Commerce Order System
+# Scenario:
+# Store prices, customer details, and orders.
+
+# Question:
+# Use vector, list, and data frame.
+
+prices <- c(1200, 850, 560)
+
+customer <- list(
+  CustomerID = 301,
+  Name = "Bhavya",
+  City = "Chennai"
+)
+
+orders <- data.frame(
+  Item = c("Phone", "Headset", "Charger"),
+  Price = prices
+)
+
+prices
+customer
+orders
