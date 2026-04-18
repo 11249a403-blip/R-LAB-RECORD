fb8f9b488f6c55d64301082515c6eb295c23d540
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 20:55:44 +0530

    Add patient details and test results using lists
    Implement patient medical record system using lists.

---
+++ EXERCISE-6/6D.R
@@ -0,0 +1,16 @@
+# 4. Patient Medical Record System (Lists)
+# Scenario:
+# Store patient details with test results.
+
+# Question:
+# Use list to store data.
+
+patient <- list(
+  PatientID = 202,
+  Name = "Bhavya",
+  Age = 20,
+  TestResults = c(120, 140, 130)
+)
+
+patient
+patient$TestResults
