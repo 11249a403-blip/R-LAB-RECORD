04aaa630a245283044372dcf957c12418ac22aec
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 19:08:45 +0530

    Create 1G.R

---
+++ EXERCISE-1/1G.R
@@ -0,0 +1,16 @@
+# 7. Movie Rating System
+# Scenario:
+# A movie app stores movie title, rating, number of reviews, and availability status.
+
+# Question:
+# Write an R program using correct data types.
+
+movie_title <- "Bahubali"
+rating <- 9.1
+reviews <- 1850
+available <- TRUE
+
+movie_title
+rating
+reviews
+available
