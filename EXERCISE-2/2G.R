25a6e434a81d2c0daaee6c960fbd8aece9f9776c
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 19:18:55 +0530

    Implement temperature warning system in R
    Add R program for temperature warning system.

---
+++ EXERCISE-2/2G.R
@@ -0,0 +1,11 @@
+# 7. Temperature Warning System
+# Question:
+# Write an R program to display appropriate warning.
+
+temperature <- -3
+
+if(temperature < 0 | temperature > 40){
+print("Extreme Temperature Warning")
+} else {
+print("Normal Temperature")
+}
