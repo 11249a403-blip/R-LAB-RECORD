5b462613988c7efec4d4ba26971cb1aaf85c8382
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 19:10:29 +0530

    Create 1H.R

---
+++ EXERCISE-1/1H.R
@@ -0,0 +1,16 @@
+# 8. Traffic Monitoring System
+# Scenario:
+# Traffic data includes vehicle count, location name, average speed, and congestion status.
+
+# Question:
+# Write an R program to store this information.
+
+vehicle_count <- 420
+location <- "City Center"
+avg_speed <- 38.5
+congested <- TRUE
+
+vehicle_count
+location
+avg_speed
+congested
