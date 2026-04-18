8fb153f7d1cdc940ca53dd15ec53767abfffc0dd
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 19:19:38 +0530

    Add grading system program in R

---
+++ EXERCISE-2/2H.R
@@ -0,0 +1,17 @@
+# 8. Grading System
+# Question:
+# Write an R program using decision making.
+
+marks <- 93
+
+if(marks >= 90){
+grade <- "A"
+} else if(marks >= 75){
+grade <- "B"
+} else if(marks >= 50){
+grade <- "C"
+} else {
+grade <- "Fail"
+}
+
+print(grade)
