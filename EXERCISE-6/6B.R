839a5a11f1a8aa46cc3e67b14909bfe04a23c961
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 20:54:33 +0530

    Create 6B.R

---
+++ EXERCISE-6/6B.R
@@ -0,0 +1,14 @@
+# 2. Temperature Monitoring System (Vectors)
+# Scenario:
+# A weather station records daily temperatures.
+
+# Question:
+# Find minimum and maximum temperature.
+
+temperature <- c(32, 33, 31, 34, 35, 36, 33)
+
+min_temp <- min(temperature)
+max_temp <- max(temperature)
+
+min_temp
+max_temp
