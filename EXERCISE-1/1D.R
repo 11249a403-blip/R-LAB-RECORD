c8d20db47b7a2a4fbd534f9c13b045e97ef6b0a0
11249a403-blip <11249a403@kanchiuniv.ac.in>
2026-04-18 18:52:05 +0530

    Create 1D.R

---
+++ EXERCISE-1/1D.R
@@ -0,0 +1,16 @@
+# 4. Weather Monitoring System
+# Scenario:
+# A weather station records temperature, city name, rainfall status, and wind speed.
+
+# Question:
+# Implement this using appropriate R data types.
+
+city <- "Hyderabad"
+temperature <- 32.7
+rain <- TRUE
+wind_speed <- 15.4
+
+city
+temperature
+rain
+wind_speed
